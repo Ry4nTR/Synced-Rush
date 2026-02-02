21ad1c4f8a34f02e543ac2562211b5e59426521e
Mar Vin <marco.tallarino@gmail.com>
2026-02-02 10:35:48 +0100

    UI Join IP window fix

--- Assets/_Scripts/UI/MainMenù/Create&Join Match/JoinMatchPanelController.cs
+++ Assets/_Scripts/UI/MainMenù/Create&Join Match/JoinMatchPanelController.cs
@@ -10,9 +10,21 @@ public class JoinMatchPanelController : MonoBehaviour
     [SerializeField] private LobbyManager lobbyManager;
     [SerializeField] private PanelManager uiManager;
 
+    [SerializeField] private CanvasGroup ipJoinWindow;
+
     private void Start()
     {
         playerNameInput.text = "";
+    }
+
+    public void ShowJoinIPWindow()
+    {
+        Show(ipJoinWindow);
+    }
+
+    public void CloseJoinIPWindow()
+    {
+        Hide(ipJoinWindow);
     }
 
     public void OnIPJoinPressed()
@@ -47,4 +59,23 @@ public class JoinMatchPanelController : MonoBehaviour
 
         uiManager.ShowLobby();
     }
+
+    private void Show(CanvasGroup group)
+    {
+        if (group == null) return;
+
+        group.alpha = 1f;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+
+    private void Hide(CanvasGroup group)
+    {
+        if (group == null) return;
+
+        group.alpha = 0f;
+        group.interactable = false;
+        group.blocksRaycasts = false;
+    }
+
 }
