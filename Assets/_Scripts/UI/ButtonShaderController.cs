92a70e71a390e568022e34e068e33cfe742e3f74
MarVin1006 <marco.tallarino@gmail.com>
2026-02-14 00:42:44 +0100

    Massively optimized buttons
    - Limited draw calls from one per button to 2
    - Also probably fixed a memory leak made by instantiating button materials

--- Assets/_Scripts/UI/ButtonShaderController.cs
+++ Assets/_Scripts/UI/ButtonShaderController.cs
@@ -2,39 +2,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ButtonShaderController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+public class ButtonShaderController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Material targetMaterial;
-    private float currentHoverValue = 0f;
-    private float targetHoverValue = 0f;
+    private Image img;
 
     [Header("Settings")]
-    public float fadeSpeed = 8f; // Più alto è, più è veloce la transizione
+    [SerializeField] private Material baseMaterial;
+    [SerializeField] private Material selectedMaterial;
 
     void Awake()
     {
         // Forza la creazione di un'istanza unica del materiale
-        Image img = GetComponent<Image>();
+        img = GetComponent<Image>();
         if (img.material != null)
         {
-            targetMaterial = new Material(img.material);
-            img.material = targetMaterial;
+            img.material = baseMaterial;
         }
     }
 
-    void Update()
-    {
-        if (targetMaterial == null) return;
-
-        // Muove linearmente il valore attuale verso quello desiderato
-        currentHoverValue = Mathf.MoveTowards(currentHoverValue, targetHoverValue, fadeSpeed * Time.deltaTime);
-
-        // Aggiorna lo shader
-        targetMaterial.SetFloat("_IsHover", currentHoverValue);
+    public void OnPointerEnter(PointerEventData eventData)
+    { 
+        img.material = selectedMaterial;
     }
-
-    public void OnPointerEnter(PointerEventData eventData) => targetHoverValue = 1f;
-    public void OnPointerExit(PointerEventData eventData) => targetHoverValue = 0f;
-    public void OnSelect(BaseEventData eventData) => targetHoverValue = 1f;
-    public void OnDeselect(BaseEventData eventData) => targetHoverValue = 0f;
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        img.material = baseMaterial;
+    }
 }
